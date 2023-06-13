83e56d549278b07fbddd20968619e09edbe949a9
mariyamax <89139747+mariyamax@users.noreply.github.com>
2023-06-13 22:08:37 +0300

    Add files via upload

--- Authorization/src/main/java/authorization/data/DataBaseConnection.java
+++ Authorization/src/main/java/authorization/data/DataBaseConnection.java
@@ -9,20 +9,11 @@ import java.sql.SQLException;
 public class DataBaseConnection {
 
     private static final String CONNECTION_ERROR = "Can not connect with database";
-    private static final String URL_PATH = "dataBaseUrl";
-    private static final String PASSWORD_PATH = "dataBasePassword";
-    private static final String USER_PATH = "dataBaseUser";
     
     private static Connection connection;
 
     public static Connection connect() throws FileNotFoundException {
         if (connection == null){
-            /*JSONParser parser = new JSONParser();
-            Object obj = parser.parse(new FileReader("src/main/resources/DataBaseSettings.json"));
-            JsonObject settings = (JsonObject) obj;
-            String url = settings.get(URL_PATH).toString();
-            String user = settings.get(USER_PATH).toString();;
-            String password = settings.get(PASSWORD_PATH).toString();;*/
             try {
                 connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/authorization","root","Petroca9052");
             } catch (SQLException e) {
